d3448d0ff7e9b2b1effaa98f135e8c3e3ab26084
Sefa DOĞAN <sefadogan7@gmail.com>
2021-02-14 19:22:20 +0300

    Changing text color

--- lib/styles/styles.dart
+++ lib/styles/styles.dart
@@ -1,6 +1,7 @@
+// Packages
 import 'package:flutter/material.dart';
 
-TextStyle contentDividerTitleTextStyle({color = const Color(0xff718096), double fontSize = 14, double height = 1.4}) {
+TextStyle contentDividerTitleTextStyle({color = Colors.grey, double fontSize = 14, double height = 1.4}) {
   return TextStyle(
     color: color,
     fontWeight: FontWeight.bold,
